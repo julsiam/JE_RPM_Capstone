b2e92f897d51e938e90a14c484cc254157b65bea
julsiam <julsestorco031602@gmail.com>
2023-10-12 16:03:02 +0800

    Added seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,25 +14,11 @@ class UserSeeder extends Seeder
     public function run()
     {
         $users = [
-            // [
-            //     'first_name' => 'Juls',
-            //     'last_name' => 'Estorco',
-            //     'phone_number' => 9514791515,
-            //     'email' => 'julsestorco031602@gmail.com',
-            //     'age' => 21,
-            //     'birthdate' => '2002-03-16',
-            //     'gender' => 'Male',
-            //     'address' => 'Cebu City, Cebu',
-            //     'occupation' => 'IT Professional',
-            //     'work_address' => "Cebu City",
-            //     'password' => bcrypt('juls12345'),
-            //     'type' => 0
-            // ],
             [
                 'first_name' => 'Christine',
                 'last_name' => 'Toledo',
                 'phone_number' => 9554800304,
-                'email' => '21103811@usc.edu.ph',
+                'email' => 'je.rentals2023@gmail.com',
                 'age' => 28,
                 'birthdate' => '1995-02-15',
                 'gender' => 'Female',

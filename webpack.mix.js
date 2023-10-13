1c5609159d60874de7b13727bc9b6405da59c61f
julsiam <julsestorco031602@gmail.com>
2023-10-13 13:17:14 +0800

    edited script

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,3 @@ const mix = require('laravel-mix')
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
-   .version()
-   .setResourceRoot('https://jerpmcapstone-production.up.railway.app/');

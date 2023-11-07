cd506242f58400e1c8553ec7e32022f5eb5d21f9
julsiam <julsestorco031602@gmail.com>
2023-11-07 14:26:37 +0800

    initial deployment using render

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "generating application key..."
+php artisan key:generate --show
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+# Run migrations (if you need to)
+echo "Running migrations..."
+php artisan migrate --force
+
+# Build your assets using npm and Laravel Mix
+# This assumes you are using Laravel Mix for asset compilation
+echo "Building assets..."
+npm install
+npm run production
+
+# Optionally, you can run other Laravel commands here as needed for your application.
+
+
+echo "Running database seeding..."
+php artisan db:seed --class=UserSeeder
+
+# Storage link (if needed)
+echo "Creating storage link..."
+php artisan storage:link

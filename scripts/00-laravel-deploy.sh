c9cf635d3d7eb5b68df254e36197ce6cc8d8c984
julsiam <julsestorco031602@gmail.com>
2023-11-13 09:10:17 +0800

    edit directory"

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,7 @@ echo "Running composer"
 
 composer global require hirak/prestissimo
 
-composer install --no-dev
+composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
 php artisan config:cache

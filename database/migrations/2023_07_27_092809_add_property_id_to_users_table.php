8702314521b7afe5a00cc57ca51fb72a77052c37
julsiam <julsestorco031602@gmail.com>
2023-11-21 17:06:47 +0800

    added property_id

---
+++ database/migrations/2023_07_27_092809_add_property_id_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('property_id')->nullable(); // Add this column
+            $table->foreign('property_id')->references('id')->on('properties')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['property_id']);
+            $table->dropColumn('property_id');
+        });
+    }
+};
